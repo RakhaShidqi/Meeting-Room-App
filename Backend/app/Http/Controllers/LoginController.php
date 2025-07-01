e4f2ad32e8be7ee6618439078289ebb429613f7a
RakhaShidqi <rakhashidqi628@gmail.com>
2025-07-02 01:04:20 +0700

    Add validation On Login Form

---
+++ Backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User; 
+
+class LoginController extends Controller
+{
+    public function view()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        // Validasi Input
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Cek Kredensial
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect()->intended('/home');
+        }
+
+        // Jika gagal
+        return back()->withErrors(['email' => 'Email atau password salah']);
+    }
+}
