1afd6a44ce63edeb0f528d652849a2c57f111caf
RakhaShidqi <rakhashidqi628@gmail.com>
2025-07-02 00:22:34 +0700

    Linked Frontend to Backend

---
+++ Backend/resources/views/forgot-password.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meeting Room App</title>
+    <link rel="icon" type="image/x-icon" href="../img/meeting-room.png ">
+    <link rel="stylesheet" href="css/lupapw.css">
+</head> 
+<body>
+<div class="circle">
+    <img src="../icon/exclamation-solid.svg" alt="exclamation icon" />
+  </div>
+
+    <div class="container">
+        <form action="">
+            <h2>Password Reset</h2>
+            <p>Masukan email Anda dan kami akan mengirimkan tautan untuk mengatur ulang kata sandi Anda</p>
+            <br><br>
+            <input type="email" name="" id="email" placeholder="         Email ID">
+            <br><br>
+            <br><br>
+            <button type="submit" class="sub">Submit</button>
+            <br><br>
+            <a href="http://127.0.0.1:8000/login">Back To Home</a>
+        </form>
+    </div>
+</body>
+</html>
